317d34ba7286b54d20fee8bc96e7ebf5056fed9d
João Victor Santos <jvssadm@gmail.com>
2021-04-28 16:31:17 -0300

    Create media.py

---
+++ media.py
@@ -0,0 +1,24 @@
+# Média escolar:
+aluno = input('Digite o nome do aluno: ')
+a = int(input('Primeiro bimestre: '))
+if a > 10:
+    a = int(input('Número digitado errado. Primeiro bimestre: '))
+b = int(input('Segundo bimestre: '))
+if b > 10:
+    b = int(input('Número digitado errado. Segundo bimestre: '))
+c = int(input('Terceiro bimestre: '))
+if c > 10:
+    c = int(input('Número digitado errado. Terceiro bimestre: '))
+d = int(input('Quarto bimestre: '))
+if d > 10:
+    d = int(input('Número digitado errado. Primeiro bimestre: '))
+media = (a + b + c + d) / 4
+soma = a + b + c + d
+if a <= 10 and b <=10 and c <=10 and d <=10:
+    print('Média: {}'.format(media))
+    if media <= 7:
+        print('A média de {} foi {}. Status: REPROVADO.'.format(aluno, media))
+    else:
+        print('A média de {} foi {}. Status: APROVADO.'.format(aluno, media))
+else:
+    print('Há uma nota errada. Conserte')
